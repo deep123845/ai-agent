7db4304196ee5f83831512737549344da130736a
deep123845 <deep123845@gmail.com>
2026-01-05 17:57:28 -0500

    added an argument to set prompt

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
         raise RuntimeError("No api key found")
+    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
 
     client = genai.Client(api_key=api_key)
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    prompt = args.user_prompt
     response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
 
     if response.usage_metadata == None:
