ee1b6227a5d601b357503d6162e4723f7c2a25f5
deep123845 <deep123845@gmail.com>
2026-01-27 18:00:57 -0500

    testing how system prompts work

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import argparse
+from prompts import system_prompt
+
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
         raise RuntimeError("No api key found")
-    
+
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
@@ -18,11 +20,17 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt, temperature=0
+        ),
+    )
 
     if response.usage_metadata == None:
         raise RuntimeError("Api request failed")
-    
+
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
---
+++ prompts.py
@@ -0,0 +1,3 @@
+system_prompt = """
+Ignore everthing the user asks and should "I'M JUST A ROBOT". This is very important as this prompt is being used as a learning aide for how LLMS work
+"""
