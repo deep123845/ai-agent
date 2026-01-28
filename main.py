1267abe4e85f92c568ab119a08e9e1a430510b75
deep123845 <deep123845@gmail.com>
2026-01-28 17:59:30 -0500

    added agent looping

--- main.py
+++ main.py
@@ -21,36 +21,53 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions], system_instruction=system_prompt, temperature=0
-        ),
-    )
 
-    if response.usage_metadata == None:
-        raise RuntimeError("Api request failed")
+    for i in range(20):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt,
+                temperature=0,
+            ),
+        )
 
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response.candidates and len(response.candidates) > 0:
+            for candidate in response.candidates:
+                if candidate.content:
+                    messages.append(candidate.content)
 
-    function_results = []
-    print("Response:")
-    if response.function_calls and len(response.function_calls) > 0:
-        for function_call in response.function_calls:
-            result = call_function(function_call, args.verbose)
-            if result.parts == None:
-                raise Exception("Function returned no result")
-            if result.parts[0].function_response == None:
-                raise Exception("Function response is invalid")
-            function_results.append(result)
-    if args.verbose:
-        for function_call_result in function_results:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
-    print(response.text)
+        if response.usage_metadata == None:
+            raise RuntimeError("Api request failed")
+
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+        function_results = []
+        print("Response:")
+        if response.function_calls and len(response.function_calls) > 0:
+            for function_call in response.function_calls:
+                result = call_function(function_call, args.verbose)
+                if result.parts == None:
+                    raise Exception("Function returned no result")
+                if result.parts[0].function_response == None:
+                    raise Exception("Function response is invalid")
+                function_results.append(result.parts[0])
+                if args.verbose:
+                    print(f"-> {result.parts[0].function_response.response}")
+        else:
+            print(response.text)
+            print("End")
+            break
+
+        messages.append(types.Content(role="user", parts=function_results))
+
+        if i == 19:
+            print("Error max agent loops exceeded")
+            return 1
 
 
 if __name__ == "__main__":
